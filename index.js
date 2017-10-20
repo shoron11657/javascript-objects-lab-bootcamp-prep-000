c50ff55fcc5214660cc55e7718c70c386cc1958f
Kevin M <shoron@gmail.com>
2017-10-20 02:44:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function deleteFromObjectByKey(recipes, prop) {
   delete recipe2.prop
   return recipe2
 }
-function destructivelyDeleteFromObjectByKey(object,key){delete object['key']
-  return object
+function destructivelyDeleteFromObjectByKey(){delete recipes['prop']
+  return recipes
 }
